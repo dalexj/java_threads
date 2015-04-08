a63c5c5d105b3971465d58b47be0d70f55d319c4
Alex <96dalex@gmail.com>
2015-04-07 21:56:06 -0600

    simple threading

--- main.java
+++ main.java
@@ -1,5 +1,14 @@
-class Main {
+public class Main extends Thread{
+  public void run() {
+    for (int i = 0; i < 1000000; i++) {
+      if(i % 100000 == 0) System.out.println(i / 100000);
+    }
+  }
+
   public static void main(String[] args) {
-    System.out.println("testing");
+    (new Main()).start();
+    (new Main()).start();
+    (new Main()).start();
+    (new Main()).start();
   }
 }
